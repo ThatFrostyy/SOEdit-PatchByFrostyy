fe23c15f1e44da9854880af8644bb42ff2a3b9bf
Frostyy <juniorjegej@gmail.com>
2026-04-14 08:34:37 +0200

    half patch

--- CSequence.h
+++ CSequence.h
@@ -9,74 +9,85 @@
 #ifndef _CSEQUENCE_
 #define _CSEQUENCE_
 
+// A single anim event entry: { frameNum "eventName" }
+struct AnimEvent
+{
+    int   frame;
+    char* name;
+};
+
 class CSequence
-   {
-    public:
-	   CSequence();
-       CSequence(CSdl *sdl);
-	   CSequence(CSequence *anm);
-       ~CSequence();
+{
+public:
+    CSequence();
+    CSequence(CSdl* sdl);
+    CSequence(CSequence* anm);
+    ~CSequence();
 
-       void   ParseFile();
-       char  *GetName();
-	   void   SetStore();
-       void   SetName(char *);
-       void   SetName();
-       void   SetFile();
-	   void   SetFile(char *name);
-       void   SetSpeed();
-	   void   SetSmooth();
-	   void   SetEvents();
-       void   SetResume();
-       void   SetAutoStart();
+    void   ParseFile();
+    char* GetName();
+    void   SetStore();
+    void   SetName(char*);
+    void   SetName();
+    void   SetFile();
+    void   SetFile(char* name);
+    void   SetSpeed();
+    void   SetSmooth();
+    void   SetEvents();
+    void   SetResume();
+    void   SetAutoStart();
 
-       CSequence *next;
-       CSequence *prev;
+    CSequence* next;
+    CSequence* prev;
 
-       CAnimBone *m_AnimBone;
+    CAnimBone* m_AnimBone;
 
-       CSdl  *m_sdl;
-       char  *m_Name;
-       char  *m_FileName;
-       float  m_Speed;
-       float  m_Smooth;
-       bool   m_Store;
-	   bool   m_Resume;
-       bool   m_Autostart;
+    CSdl* m_sdl;
+    char* m_Name;
+    char* m_FileName;
+    float  m_Speed;
+    float  m_Smooth;
+    bool   m_Store;
+    bool   m_Resume;
+    bool   m_Autostart;
 
-    private:
-	   CString msg_st;
-   };
+    // Preserved anim events (parsed from {events ...} block)
+    AnimEvent* m_Events;
+    int        m_EventCount;
+
+private:
+    CString msg_st;
+};
 
 
 class CSequenceList
-   {
-    public:
-       CSequence *m_head;
-       CSequence *m_tail;
-       CSequence *m_curr;
+{
+public:
+    CSequence* m_head;
+    CSequence* m_tail;
+    CSequence* m_curr;
 
-	   CSequenceList();
-       CSequenceList(CSdl *sdl);
-	   CSequenceList(CSequenceList *anm);
-       ~CSequenceList();
+    CSequenceList();
+    CSequenceList(CSdl* sdl);
+    CSequenceList(CSequenceList* anm);
+    ~CSequenceList();
 
-       CSequence *AddToHead();
-       CSequence *AddToTail();
-	   CSequence *AddToTail(CSequence *anm);
-       CSequence *GetFirst();
-       CSequence *GetNext();
-	   CSequence *FindByName(char *szName);
-	   void DropSequence(char *szName);
-       CSequence *GetPrev();
-       CSequence *GetCurr();
-       void       ClearList();
-       CSequence *GetLast();
+    CSequence* AddToHead();
+    CSequence* AddToTail();
+    CSequence* AddToTail(CSequence* anm);
+    CSequence* GetFirst();
+    CSequence* GetNext();
+    CSequence* FindByName(char* szName);
+    void DropSequence(char* szName);
+    CSequence* GetPrev();
+    CSequence* GetCurr();
+    void       ClearList();
+    CSequence* GetLast();
 
-       void       WriteMdl(FILE *fp, int indent);
+    void       WriteMdl(FILE* fp, int indent);
 
-       CSdl      *m_sdl;
-   };
+    CSdl* m_sdl;
+};
 
 
 #endif
